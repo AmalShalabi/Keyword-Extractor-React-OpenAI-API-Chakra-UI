3bfb1b06cfce88b9c652ada53fbe2a31e03eb450
unknown <mouni.shalaby@gmail.com>
2023-04-01 21:04:19 +0300

    TextInPut component

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import { Container, Box } from "@chakra-ui/react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import TextInPut from "./components/TextInPut";
 
 const App = () => {
   return (
-    <Box bg="blue.600" color="white" height="100vh" paddingTop={130}>
-      <Container maxW='3xl' centerContent>
-        <Header/>
-        <Footer/>
+    <Box bg="blue.800" color="white" height="100vh" paddingTop={130}>
+      <Container maxW="3xl" centerContent >
+        <Header />
+        <TextInPut />
+        <Footer  />
       </Container>
     </Box>
   );
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,12 +3,10 @@ import logo from '../assets/openai.png'
 
 const Footer = () => {
   return (
-    <Box marginTop={50}>
+    <Box marginTop={50} >
       <Flex justifyContent="center" alignItems="center">
-        <Image src={logo} marginRight={1} />
-        <Text>
-            Powered By Open AI
-        </Text>
+        <Image src={logo} marginRight={1} width={5} borderRadius="md" />
+        <Text  color="gray.800">Powered By Open AI</Text>
       </Flex>
     </Box>
   );
