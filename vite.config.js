cceaeeadac9e8b7b337ffdee3e1e6f3e0371210a
unknown <mouni.shalaby@gmail.com>
2023-04-02 12:58:10 +0300

    delete dist dir

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Keyword-Extractor-React-OpenAI-API-Chakra-UI/",
   plugins: [react()],
   ports: 3000,
 });
